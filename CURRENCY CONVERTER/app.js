8e6817e339b18e339c3203079d11fcd735a3a211
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-21 00:08:32 +0530

    Update app.js

--- CURRENCY CONVERTER/app.js
+++ CURRENCY CONVERTER/app.js
@@ -1,25 +1,34 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json"
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-const dropdowns = document.querySelectorAll(".dropdown select")
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 
 for (let select of dropdowns) {
-    for(currCode in countryList) {
-        let newOption = document.createElement("option")
-        newOption.innerText = currCode;
-        newOption.value = currCode;
-        if(select.name === "from" && currCode === "USD") {
-            newOption.selected = "selected";
-        } else if(select.name === "to" && currCode === "INR")  {
-            newOption.selected = "selected"
-          }
-        select.append(newOption)
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
     }
-}
+    select.append(newOption);
+  }
 
-select.addEventListner("change" , (evt) =>{ 
- updateFlag(evt.target);
-})
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
 
-const updateFlag = (element) => {
- console.log(element)
+  const updateFlag = (element) =>{
+    let currCode = element.value
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img = element.parentElement.querySelector("img")
+    img.src = newSrc;
+  }
 }
