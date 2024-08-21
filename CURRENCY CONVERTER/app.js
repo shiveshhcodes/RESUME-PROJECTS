4334830c67304775166c2229a5ae61610ed2f4ec
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-21 21:49:00 +0530

    Update app.js

--- CURRENCY CONVERTER/app.js
+++ CURRENCY CONVERTER/app.js
@@ -1,43 +1,60 @@
+// import currencyapi from '@everapi/currencyapi-js'
+
+// const BASE_URL = new currencyapi('https://api.currencyapi.com/v3/latest?apikey=cur_live_eXBWRZSPwxGoKthBOfQ2nDjd4HwkCKzuuLlsIOpM')
+// BASE_URL.latest({
+//     base_currency: 'USD',
+//     currencies: 'EUR'
+// }).then(response => {
+//     console.log(response)
+// });
+
+
+// const dropdowns = document.querySelectorAll(".dropdown select");
+// const btn = document.querySelector("form button");
+// const fromCurr = document.querySelector(".from select");
+// const toCurr = document.querySelector(".to select");
+// const msg = document.querySelector(".msg");
+
+// for (let select of dropdowns) {
+//   for (currCode in countryList) {
+//     let newOption = document.createElement("option");
+//     newOption.innerText = currCode;
+//     newOption.value = currCode;
+//     if (select.name === "from" && currCode === "USD") {
+//       newOption.selected = "selected";
+//     } else if (select.name === "to" && currCode === "INR") {
+//       newOption.selected = "selected";
+//     }
+//     select.append(newOption);
+//   }
+
+//   select.addEventListener("change", (evt) => {
+//     updateFlag(evt.target);
+//   });
+
+//   const updateFlag = (element) =>{
+//     let currCode = element.value
+//     let countryCode = countryList[currCode];
+//     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+//     let img = element.parentElement.querySelector("img")
+//     img.src = newSrc;
+//   }
+// };
+// btn.addEventListener("click" , async (evt) => {
+//  evt.preventDefault(); 
+//   let amount = document.querySelector(".amount input");
+//   let amtVal = amount.value;
+//   if(amtVal === "" || amtVal<1) {
+//     amtVal = 1;
+//     amount.value = 1
+//   }
+//   const URL = BASE_URL
+//   let response = await fetch(URL , options);
+//   let data = await response.json();
+//   console.log(data)
+// });
+
+
 const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-const dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector("form button");
-const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector(".to select");
-const msg = document.querySelector(".msg");
-
-for (let select of dropdowns) {
-  for (currCode in countryList) {
-    let newOption = document.createElement("option");
-    newOption.innerText = currCode;
-    newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
-      newOption.selected = "selected";
-    } else if (select.name === "to" && currCode === "INR") {
-      newOption.selected = "selected";
-    }
-    select.append(newOption);
-  }
-
-  select.addEventListener("change", (evt) => {
-    updateFlag(evt.target);
-  });
-
-  const updateFlag = (element) =>{
-    let currCode = element.value
-    let countryCode = countryList[currCode];
-    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
-    let img = element.parentElement.querySelector("img")
-    img.src = newSrc;
-  }
-};
-btn.addEventListener("click" , (evt) => {
-    evt.preventDefault(); 
-    let amount = document.querySelector(".amount input");
-    let amtVal = amount.value;
-    if(amtVal === "" || amtVal<1) {
-        amtVal = 1;
-        amount.value = 1
-    }
-});
