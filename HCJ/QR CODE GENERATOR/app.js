ba7d13aea9ade075c960663a59f3b80a6032d6cf
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-25 01:49:20 +0530

    Update app.js

--- HCJ/QR CODE GENERATOR/app.js
+++ HCJ/QR CODE GENERATOR/app.js
@@ -3,6 +3,10 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQR(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-    imgBox.classList.add("show-img"); 
-}
+    if(qrText.value.lenght > 0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" 
+        + qrText.value;
+        imgBox.classList.add("show-img"); 
+    }
+    
+} 
