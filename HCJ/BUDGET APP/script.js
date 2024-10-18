25bd74aa41028e2eb185b15a00c378869d36caaa
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-10-19 00:05:36 +0530

    Update script.js

--- HCJ/BUDGET APP/script.js
+++ HCJ/BUDGET APP/script.js
@@ -5,6 +5,12 @@ const totalAmountButton = document.getElementById("total-amount-button");
 const productTitle = document.getElementById("product-title");
 const errorMessage = document.getElementById("budget-error");
 const productTitleError = document.getElementById("product-title-error");
+const productCostError = document.getElementById("product-cost-error");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+let tempAmount = 0;
 
 totalAmountButton.addEventListener("click", () => {
     tempAmount = totalAmount.value;
