a8a66c15e0b322260ae6a7208ce22b77d4cb3191
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2025-02-15 23:09:24 +0530

    Update script.js

---
+++ Frontend Projects/STUDENT DASHBOARD/script.js
@@ -0,0 +1,6 @@
+const recordForm = document.getElementById('record-form');
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const emailInput = document.getElementById('email');
+const recordList = document.getElementById('record-list');
+const editIndexInput = document.getElementById('edit-index');
